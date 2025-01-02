249a5004dd272a0a1d7c0154cfe451d20f51d95e
Wade Jones <wadedjones@gmail.com>
2025-01-01 22:17:05 -0800

    re-added nvim config

---
+++ nvim/.config/nvim/lua/plugins/autoformat.lua
@@ -0,0 +1,25 @@
+-- autoformat.lua
+--
+-- Use your language server to automatically format your code on save.
+-- Adds additional commands as well to manage the behavior
+
+return {
+  'stevearc/conform.nvim',
+  opts = {
+    notify_on_error = false,
+    format_on_save = {
+      timeout_ms = 500,
+      lsp_fallback = true,
+    },
+    formatters_by_ft = {
+      lua = { 'stylua' },
+      c = { 'clangd' },
+      -- Conform can also run multiple formatters sequentially
+      -- python = { "isort", "black" },
+      --
+      -- You can use a sub-list to tell conform to run *until* a formatter
+      -- is found.
+      -- javascript = { { "prettierd", "prettier" } },
+    },
+  },
+}
---
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,74 @@
+return {
+    'nvim-treesitter/nvim-treesitter',
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter-textobjects',
+    },
+    build = ':TSUpdate',
+		config = function()
+			require('nvim-treesitter.configs').setup {
+				ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'typescript', 'vimdoc', 'vim', 'graphql' },
+
+				-- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
+				auto_install = true,
+
+				modules = {},
+				sync_install = false,
+				ignore_install = {},
+				highlight = { enable = true },
+				indent = { enable = true },
+				incremental_selection = {
+					enable = true,
+					keymaps = {
+						init_selection = '<c-space>',
+						node_incremental = '<c-space>',
+						scope_incremental = '<c-s>',
+						node_decremental = '<M-space>',
+					},
+				},
+				textobjects = {
+					select = {
+						enable = true,
+						lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+						keymaps = {
+							-- You can use the capture groups defined in textobjects.scm
+							['aa'] = '@parameter.outer',
+							['ia'] = '@parameter.inner',
+							['af'] = '@function.outer',
+							['if'] = '@function.inner',
+							['ac'] = '@clss.outer',
+							['ic'] = '@class.inner',
+						},
+					},
+					move = {
+						enable = true,
+						set_jumps = true, -- whether to set jumps in the jumplist
+						goto_next_start = {
+							[']m'] = '@function.outer',
+							[']]'] = '@class.outer',
+						},
+						goto_next_end = {
+							[']M'] = '@function.outer',
+							[']['] = '@class.outer',
+						},
+						goto_previous_start = {
+							['[m'] = '@function.outer',
+							['[['] = '@class.outer',
+						},
+					},
+					goto_previous_end = {
+						['[M'] = '@function.outer',
+						['[]'] = '@class.outer',
+					},
+				},
+				swap = {
+					enable = true,
+					swap_next = {
+						['<leader>a'] = '@parameter.inner',
+					},
+					swap_previous = {
+						['<leader>A'] = '@parameter.inner',
+					},
+				},
+			}
+		end
+			}
